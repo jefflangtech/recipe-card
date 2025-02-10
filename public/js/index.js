e9f4e815933645be60a2113f21aca5621c69622c
Jeff Lang <jeff.m.lang@gmail.com>
2025-02-10 14:36:44 -0800

    Completed mobile styling

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ document.addEventListener("keydown", (event) => {
   if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === 'u') {
       event.preventDefault();
       if (overlay) {
+        console.dir(overlay);
         overlay.hidden = !overlay.hidden;
       }
   }
